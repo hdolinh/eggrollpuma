d6aad3d3a8fca1000317af59b9fba40c47763881
hdolinh <halina@bren.ucsb.edu>
2021-08-13 11:54:52 -0700

    Initial commit

---
+++ R/puma_fact.R
@@ -0,0 +1,20 @@
+# Write a function for your package
+
+#' Tells you a Puma Fun Fact
+#'
+#' Contains one function about a specific puma based on their name, weight, and height.
+#'
+#' @param puma_name A character containing the input puma name.
+#' @param weight A number that indicates weight of the puma specified by puma name.
+#' @param height A number that indicates height of the puma specified by puma name.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+puma_fact <- function(puma_name, weight, height) {
+  puma_body_bmi <- (weight / height)
+  return(paste(puma_name, "has a BMI of", puma_body_bmi))
+}
+
+puma_fact(puma_name = "P22", weight = 120, height = 13)
